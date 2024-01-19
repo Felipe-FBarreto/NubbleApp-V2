bd837d12422af126574ccc8de7ae56be2a3d6147
Felipe Barreto <felipebarreto1050@gmail.com>
2024-01-19 14:57:51 -0300

    Feature: Created component theme in restyle

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
+import {Button} from './src/components/Button/Button';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <Text preset="headingLarge">Projeto Nubble</Text>
-      <Text preset="headingMedium">Projeto Nubble</Text>
-      <Text preset="paragraphLarge" semiBold>
-        Projeto Nubble
-      </Text>
+      <ThemeProvider theme={theme}>
+        <View style={{paddingHorizontal: 24}}>
+          <Text preset="headingLarge">Projeto Nubble</Text>
+          <Text preset="headingMedium">Projeto Nubble</Text>
+          <Text preset="paragraphLarge" semiBold>
+            Projeto Nubble
+          </Text>
+          <Button title="Entrar" />
+        </View>
+      </ThemeProvider>
     </SafeAreaView>
   );
 }
